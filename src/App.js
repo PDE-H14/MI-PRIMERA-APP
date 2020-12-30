46977a9e65d1cccb161feac558715ad224a730fe
PDE-H14 <escamilla.huerta.porfirio@gmail.com>
2020-12-30 15:39:31 -0600

    Utilizando useState

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
+import React, {useState} from "react";
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+    const [stateCar, setStateCar] = useState(false);
+    const encenderApagar = () => {
+        //setStateCar(!stateCar);
+        setStateCar(prevValue => !prevValue);
+    }
     return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <h3>El coche está: {stateCar ? "Encendido" : "Apagado"}</h3>
+        <button onClick={encenderApagar}>Encender/Apagar</button>
       </header>
     </div>
   );
